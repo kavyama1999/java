9fc59306aa977230ac515b43873348b4eb038924
kavyama1999 <kavyama.xworkz@gmail.com>
2023-12-06 12:05:23 +0530

    Declaring Methods

---
+++ Oracle.java
@@ -0,0 +1,51 @@
+public class Oracle{
+public static void main(String args[]){
+System.out.println("Welcome");
+car();
+run();
+update();
+}
+
+ static void car(){
+System.out.println("Kia");
+Play();
+System.out.println("Eritiga");
+pop();
+}
+ static void Play()
+{
+	System.out.println("Playing Badminton");
+}
+
+static void run(){
+	System.out.println("Running");
+	joy();
+}
+
+static void joy(){
+	
+	System.out.println("Happy");
+	System.out.println("Joliness");
+}
+static void pop()
+{
+	System.out.println("Pop it");
+}
+
+static void update()
+{
+	vote();
+	System.out.println("updating");
+}
+
+static void vote()
+{
+	System.out.println("voting");
+	System.out.println("BJP");
+	stop();
+}
+static void stop()
+{
+	System.out.println("Stop it");
+}
+}
