438970af08e8fb0aaff06c4357782cdc0fd144a4
kavyama1999 <kavyama.xworkz@gmail.com>
2023-12-14 14:43:33 +0530

    fro loop

---
+++ Calculator.java
@@ -0,0 +1,59 @@
+public class Calculator{
+public static void main(String args[])
+{
+int intArray[]={10000,25000,20000,4780,5340,6120,7340,8340,9230,1456,2987,2343,4590,67345,78233,6345,43453,23344,53444,2365};
+for(int i=0;i<20;i++)
+System.out.println( "values " + intArray[i]);
+System.out.println(intArray[5]);
+
+
+	
+char charArray[]={'k','l','m','n','y','s','o','w','q','a','x','c','v','b','z','t','u','i','f','d'};
+for (int i=0;i<20;i++)
+System.out.println( charArray[i]);
+System.out.println(charArray[4]);
+
+short shortArray[]={(short)15000,(short)25000,(short)1000,(short)2000,(short)3000,(short)25000,(short)23000,(short)12000,(short)22000,(short)31000,(short)26000,(short)14000,(short)15000,(short)8000,(short)31000,(short)2500,(short)7800,(short)4520,(short)3654,(short)28000};
+    for(int i=0;i<20;i++)
+	System.out.println("short " + shortArray[i]);
+	
+
+
+boolean[] booleanArray={true,false,true,false,true,false,true,false,true,true,false,false,true,false,true,false,true,false,true,false};
+for(int i=0;i<20;i++)
+	System.out.println( booleanArray[i]);
+System.out.println(booleanArray[4]);
+
+byte[] byteArray={(byte)120,(byte)122,(byte)114,(byte)145,(byte)123,(byte)129,(byte)124,(byte)111,(byte)122,(byte)124,(byte)124,(byte)125,(byte)12,(byte)90,(byte)24,(byte)45,
+(byte)56,(byte)67,(byte)78,(byte)89};
+for( int i=0;i<20;i++)
+
+	System.out.println(byteArray[i]);
+
+long[] longArray={125698693l,1569846l,2569874l,2659874l,5896589l,12547896l,36945874l,1569847l,1236598l,12366985l,125698693l,15698416l,256982574l,89659825l,458925658l,523695258l,45892568l,12253658l,15986725l,45256985l};
+	for(int i=0;i<20;i++)
+	{
+	System.out.println(longArray[i]);
+	}
+
+float[] floatArray={2.8f,2.5f,5.6f,5.9f,16.5f,25.6f,14.2f,25.1f,36.5f,9.5f,5.3f,4.2f,3.2f,4.3f,6.3f,12.2f,36.2f,8.2f,36.1f,56.2f};
+	for(int i=0;i<20;i++)
+	{
+	System.out.println(floatArray[i]);
+	}
+	
+double doubleArray[]={1.253654d,2.36598d,2.3697d,6.2568d,4.2658d,2.6587d,3.2568d,2.1458d,4.2568f,5.6988d,3.25698d,1.253654d,2.36598d,2.3697d,
+6.2568d,4.2858d,2.7887d,3.2568d,2.1458d,4.2568d};
+	for( int i=0;i<20;i++)
+	{
+
+		System.out.println(doubleArray[i]);
+	}
+	
+
+}
+
+}
+
+
+
